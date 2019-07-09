7923a5da5bd7dbb68a2035db162015be26d3875f
marylinkana <kanabaudoin7@gmail.com>
2019-07-09 17:21:14 +0200

    inscription, identificatio, connection, deconnexion,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bibliotheque';
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyBRK840Le6I5bXa3dtbCG-UgIpQh1VO3ns",
+      authDomain: "bibliotheque-8ff88.firebaseapp.com",
+      databaseURL: "https://bibliotheque-8ff88.firebaseio.com",
+      projectId: "bibliotheque-8ff88",
+      storageBucket: "",
+      messagingSenderId: "489341297639",
+      appId: "1:489341297639:web:d6b29d80b1e7e3cc"
+    };
+    firebase.initializeApp(config);
+  }
+
 }
