06f24f9562753fef668d3322123ed86aceab5b0f
marylinkana <kanabaudoin7@gmail.com>
2019-07-10 16:26:00 +0200

    create methodes of book service

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Book } from '../models/Book.model';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
+
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/bools').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+    .on('value', (data) => {
+      this.books = data.val() ? data.val() : [];
+      this.emitBooks();
+    });
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books'+id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexRemove = this.books.findIndex(
+      (bookElement) => {
+        if (bookElement === book){
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 
   constructor() { }
 }
